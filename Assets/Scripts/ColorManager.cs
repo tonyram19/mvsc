e706c0b9bb554250775fd23e88d2e784a12e10e2
Tony Ramirez <tonyram20@gmail.com>
2016-03-19 18:37:39 -0430

    Intial commit

---
+++ Assets/Scripts/ColorManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorManager : MonoBehaviour
+{
+    public Color[] colors;
+
+    public Color GenerateRandomColor()
+    {
+        Color color = new Color();
+
+        int random = Random.Range(0, colors.GetLength(0) - 1);
+        color = colors[random];
+
+        return color;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public ColorManager colorManager;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
